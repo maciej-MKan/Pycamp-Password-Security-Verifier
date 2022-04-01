64e117e0083f67959d4f64bd3bd3e0099c63bec6
Maciej <kanabaj.maciej@gmail.com>
2022-04-02 01:03:34 +0200

    add support for argv

--- fuse.py
+++ fuse.py
@@ -1,4 +1,5 @@
 from os.path import isfile
+from sys import argv
 from validator import Validator
 from passwd import Passwd
 from leak_check import LeakCheck
@@ -18,22 +19,80 @@ class InputFormer:
         return None
 
 class PasswordChecker:
-    def __init__(self, passwords : str) -> None:
+    def __init__(self, passwords : str, valid_exc : bool = False) -> None:
         self.passwords = InputFormer(passwords)
-        self.check_valid_and_leak()
+        self.valic_exc = valid_exc
 
     def check_valid_and_leak(self):
+        safe = None
         for password in self.passwords.get_data():
             passwd = Passwd(password.strip())
-            if Validator(passwd):
-                if LeakCheck(passwd):
-                    print('is ok')
+            try:
+                if Validator(passwd, raise_exceptions = self.valic_exc):
+                    if LeakCheck(passwd):
+                        safe = 'is safe'
+                    else:
+                        safe = 'not safe - leak'
                 else:
-                    print('leak')
-            else:
-                print('not valid')
+                    safe = 'not valid'
+                yield passwd.raw_pass, safe
+            except Exception as err:
+                yield passwd.raw_pass, err
+
+class Fuse:
+    def __init__(self) -> None:
+        self.passwd_output = False
+        self.log = False
+
+    def check_start_parameters(self):
+        if '-log' in argv:
+            self.log = True
+        if '-output' in argv:
+            self.passwd_output = True
+        for arg in argv[1:]:
+            if isfile(arg):
+                return arg
+        return None
+
+    def get_data_from_user(self):
+        input_data = input("Give me password's file or single password to check: ")
+        return input_data
+
+    def get_parameters_from_user(self):
+        user_input = None
+        while not user_input in ['Y','y','N','n']:
+            user_input = input('Do you wont make output file with safety passwords? (y/n): ')
+            if user_input.lower() == 'y':
+                    self.passwd_output = True
+        user_input = None
+        while not user_input in ['Y','y','N','n']:
+            user_input = input('Turn on Logs? (y/n): ')
+            if user_input.lower() == 'y':
+                    self.log = True
+
+    def check_password_safety(self, password_content):
+        for data, message in PasswordChecker(password_content, self.log).check_valid_and_leak():
+            if self.log:
+                self.cerate_log((data, message))
+            if self.passwd_output:
+                self.make_output_file(data)
+            yield data, message
+
+    def cerate_log(self, log_data):
+        pass
+
+    def make_output_file(self, safety_password):
+        with open('out.txt', 'w+') as output_file:
+            output_file.write(safety_password)
 
 
 if __name__ == '__main__':
-    input_data = input("Give me password's file or single password to check: ")
-    PasswordChecker(input_data)
+    fuse = Fuse()
+    input_data = None
+    if len(argv) > 1:
+        input_data = fuse.check_start_parameters()
+    else:
+        fuse.get_parameters_from_user()
+    if not input_data:
+        input_data = fuse.get_data_from_user()
+    fuse.check_password_safety(input_data)
