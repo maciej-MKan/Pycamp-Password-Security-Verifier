6dfe02294c48cd241264a2b482073f74950017f0
Maciej <kanabaj.maciej@gmail.com>
2022-04-08 21:28:47 +0200

    Completing the tests for fuse

--- test/fuse_test.py
+++ test/fuse_test.py
@@ -1,23 +1,17 @@
 """tests for fuse.py"""
 
-from cgi import test
 from sys import argv
-from unittest import mock
-from unittest.mock import patch, mock_open
+from unittest.mock import patch, mock_open, Mock
 from fuse import Fuse, InputFormer, PasswordChecker
 from validator import Validator
 from leak_check import LeakCheck
-
+#pylint: disable=too-many-arguments
 
 TEST_FILE_DATA =\
 """line1
 line2
 line3
 """
-
-def answer_generator():
-    for i in ['Y', 'N']:
-        yield i
 
 @patch('builtins.open', new_callable=mock_open, read_data=TEST_FILE_DATA)
 @patch('os.path.isfile')
@@ -80,7 +74,7 @@ def test_check_valid_and_leak_w_exc(mock_leak_check):
         ('TESTPW1#', 'the password does not have enough lowercases')
 
 @patch('os.path.isfile')
-@patch.object(Fuse, 'cerate_log')
+@patch.object(Fuse, 'create_log')
 @patch.object(Fuse, 'passwd_output_enable')
 def test_check_start_parameters_w_all_argv(mock_poe, mock_create_log, mock_isfile):
     """test check_start_parameters with all sys attribute"""
@@ -91,14 +85,14 @@ def test_check_start_parameters_w_all_argv(mock_poe, mock_create_log, mock_isfil
     argv.extend(['dumy_path_to_file', 'test_file', '-log', '-output'])
 
     test_content = test_fuse.check_start_parameters()
-    
+
     mock_poe.assert_called()
     mock_create_log.assert_called_with('Looging enabled by start parameter')
     mock_isfile.assert_called_with('test_file')
     assert test_content == 'test_file'
 
 @patch('os.path.isfile')
-@patch.object(Fuse, 'cerate_log')
+@patch.object(Fuse, 'create_log')
 @patch.object(Fuse, 'passwd_output_enable')
 def test_check_start_parameters_wo_argv(mock_poe, mock_create_log, mock_isfile):
     """test check_start_parameters with out additional sys attribute"""
@@ -108,7 +102,7 @@ def test_check_start_parameters_wo_argv(mock_poe, mock_create_log, mock_isfile):
     argv.clear()
 
     test_content = test_fuse.check_start_parameters()
-    
+
     mock_poe.assert_not_called()
     mock_create_log.assert_not_called()
     mock_isfile.assert_not_called()
@@ -135,7 +129,7 @@ def test_password_output_enable(mock_warning, mock_isfile, mock_datetime):
     test_file().write.assert_called_with('\n2022-04-08 00:00:00\n')
     assert test_fuse.passwd_output is True
 
-@patch.object(Fuse, 'cerate_log')
+@patch.object(Fuse, 'create_log')
 @patch.object(Fuse, 'passwd_output_enable')
 @patch('builtins.input')
 def test_parameters_from_user_with_both(mock_input, mock_poe, mock_log):
@@ -148,17 +142,49 @@ def test_parameters_from_user_with_both(mock_input, mock_poe, mock_log):
     mock_poe.assert_called()
     mock_log.assert_called()
 
-@patch.object(Fuse, 'cerate_log')
+@patch.object(Fuse, 'create_log')
 @patch.object(Fuse, 'passwd_output_enable')
 def test_parameters_from_user_with_log(mock_poe, mock_log):
     """test parameters_from_user when -log option are selected"""
-    test_fuse = Fuse()
-    #answer_list = ['N', 'Y']
 
-    #mock_input.return_value = answer_list.pop()
-    with patch('builtins.input') as mock_input:
-        mock_input.return_value = 'Y'
+    test_fuse = Fuse()
+
+    input_mock = Mock()
+    input_mock.side_effect = ['Y','N']
+
+    with patch('builtins.input', input_mock):
         test_fuse.get_parameters_from_user()
 
     mock_poe.assert_not_called()
     mock_log.assert_called()
+
+@patch.object(Fuse, 'make_output_file')
+@patch('fuse.logging')
+@patch.object(Validator, '__bool__')
+@patch.object(LeakCheck, '__bool__')
+@patch('builtins.open', new_callable=mock_open, read_data=TEST_FILE_DATA)
+@patch('os.path.isfile')
+@patch('fuse.datetime')
+def test_fuse_general(
+    mock_datetime, mock_isfile, mock_file, mock_leak, mock_validator, mock_logging, mock_output
+    ):
+    """general test for fuse module called with argv"""
+
+    mock_datetime.now.return_value = '2022-04-08 00:00:00'
+    mock_isfile.return_value = True
+    mock_leak.return_value = True
+    mock_validator.return_value = True
+
+    test_fuse = Fuse()
+    test_fuse.log = True
+    test_fuse.passwd_output = True
+
+    rezult = test_fuse.check_password_safety('test_file')
+
+    assert list(rezult) == [('line1', 'is safe'), ('line2', 'is safe'), ('line3', 'is safe')]
+    assert all(True for out in ['line1', 'line2', 'line3'] if out in mock_output.mock_calls)
+    assert all(
+        True for out in ['line1 - is safe', 'line2 - is safe', 'line3 - is safe']\
+            if out in mock_logging.mock_calls)
+    mock_isfile.assert_called_with('test_file')
+    mock_file.assert_called()
